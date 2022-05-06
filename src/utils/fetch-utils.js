5b9f58661edeaf395d865dc296f33fc598b1272b
James Keicher <jamesdkeicher@gmail.com>
2022-05-06 15:29:29 -0700

    feat: fetch utils to get and insert entries to db

--- src/utils/fetch-utils.js
+++ src/utils/fetch-utils.js
@@ -13,3 +13,19 @@ export const signin = async (email, password) => {
 export const logout = async () => {
   await client.auth.signOut();
 };
+
+export const getEntries = async () => {
+  const response = await client
+    .from('entries')
+    .select('*')
+    .order('created_at', { ascending: false });
+
+  return response.data;
+};
+
+export const insertEntry = async (email, content) => {
+  const response = await client
+    .from('entries')
+    .insert({ guest_id: email, content });
+  return response.data;
+};
