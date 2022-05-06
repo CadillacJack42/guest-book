209cf726a8022745b54782a72c08bc18de33ded5
James Keicher <jamesdkeicher@gmail.com>
2022-05-06 15:24:00 -0700

    feat: generic form created to add user entries

--- src/views/GuestBook.jsx
+++ src/views/GuestBook.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GuestBook = () => {
+  const [entries, setEntries] = useState([]);
+  const [newEntry, setNewEntry] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(newEntry);
+  };
+
+  const handleChange = (e) => {
+    setNewEntry(e.target.value);
+  };
+
   return (
     <>
       <h3>Welcome To Cadillac Jacks Guest Book</h3>
-      <label htmlFor="comment">Add Entry</label>
-      <input
-        type="text"
-        name="comment"
-        placeholder="Say something nice"
-      ></input>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="comment">Add Entry</label>
+        <input
+          type="text"
+          name="comment"
+          placeholder="Say something nice"
+          onChange={handleChange}
+        ></input>
+        <button>Add Entry</button>
+      </form>
+      {entries.map((entry) => {
+        return (
+          <>
+            <p>{entry}</p>
+          </>
+        );
+      })}
     </>
   );
 };
